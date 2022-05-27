116b7a348c9d3e09404ce1100c20ab10d5c08560
Jaswant Mandloi <jaswantmandloi@gmail.com>
2022-05-27 16:50:21 +0530

    my js and react project

---
+++ modules/main.js
@@ -0,0 +1,5 @@
+import {a, myDemo as Demo,funExm,} from './modules.js'
+console.log(a)
+console.log(Demo)
+
+funExm("Dinesh mandloi");
---
+++ modules/modules.js
@@ -0,0 +1,7 @@
+ let a = "welcome javascript module";
+
+const funExm = () =>{
+    console.log('i am a function')
+}
+ let myDemo = "hiiiii.....";
+export  {a,funExm,myDemo};
